962ab3423685ebcf587d45db9182f5ac5d7fa6ab
9267435@qq.com <9267435@qq.com>
2018-06-05 23:48:44 +0800

    commit

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   default_module: 'api',
   weixin: {
-    appid: '', // 小程序 appid
-    secret: '', // 小程序密钥
+    appid: 'wx1282cb3925f9bc0d', // 小程序 appid
+    secret: '8e9c886bf009d03d3186bcd2fb605d36', // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
     notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
