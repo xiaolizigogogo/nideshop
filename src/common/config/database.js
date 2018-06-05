ed6eb05c0c275fb6e74c5512d9afec3089413b06
9267435@qq.com <9267435@qq.com>
2018-06-05 23:44:38 +0800

    commit

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -5,7 +5,7 @@ module.exports = {
   database: 'nideshop',
   prefix: 'nideshop_',
   encoding: 'utf8mb4',
-  host: '127.0.0.1',
+  host: 'db',
   port: '3306',
   user: 'root',
   password: 'root',
